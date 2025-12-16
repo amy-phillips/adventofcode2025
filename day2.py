399ccd2d30900ebe00f2f6f8fd309a1982bcabe4
amy-phillips <amy@islovely.co.uk>
2025-12-16 19:59:57 +0000

    day 2 part 2

--- day2.py
+++ day2.py
@@ -2,18 +2,37 @@
 with open("day2input.txt","r") as f:
     input = f.read()
 
+def split_and_check(value_to_split: int, num_splits: int) -> bool :
+    string_i = str(value_to_split)
+    string_len = len(string_i)
+
+    # if we can't split into n parts cleanly then it's not gonna work for this i
+    if(string_len % num_splits != 0):
+        return False
+    
+    split_strings = []
+    for i in range(0, num_splits):
+        sub_string = string_i[int(i*string_len/num_splits):int((i+1)*string_len/num_splits)]
+        split_strings.append(sub_string)
+        for j in range(0, i):
+            if split_strings[i] != split_strings[j]:
+                return False
+            
+    # they all matched - it's a repeating pattern
+    return True
+
 total = 0
 for val_range in input.split(','):
     minval,maxval = val_range.split("-")
 
     for i in range(int(minval),int(maxval)+1):
-        # make the number a string, split it in half, see if the halves match
         string_i = str(i)
         string_len = len(string_i)
-        first_string = string_i[0:int(string_len/2)]
-        second_string = string_i[int(string_len/2):string_len]
 
-        if first_string == second_string:
-            total += i
+        # make the number a string, split it in n, see if the parts match
+        for j in range(2, string_len+1):
+            if split_and_check(i, j):
+                total += i
+                break
 
 print(total)
