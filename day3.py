bd69a344ca809e791d400cc0605ea9da2a09aa86
amy-phillips <amy@islovely.co.uk>
2025-12-17 07:55:24 +0000

    day 3 part 2

--- day3.py
+++ day3.py
@@ -2,43 +2,36 @@
 with open("day3input.txt","r") as f:
     input = f.read()
 
+def get_next_digit(battery: str, start_offset: int, remaining: int) -> [int,int] :
+    for high_val in range(9, 0, -1):
+        # don't look at the last few digits as then no space after it!
+        for battery_index in range(start_offset, len(battery)-remaining+1):
+            digit = int(battery[battery_index])
+
+            if digit != high_val:
+                continue
+
+            return digit, battery_index+1
+
 
 total = 0
 for battery in input.split('\n'):
     # first find the position of the highest digit
-    first_digit = 0
-    first_digit_pos = -1
-    for high_val in range(9, 0, -1):
-        if first_digit > 0:
+    digits = []
+    num_digits_wanted = 12
+    offset = 0
+    for digit_index in range(0,num_digits_wanted):
+        if len(digits)==num_digits_wanted:
             break
-
-        # don't look at the very last digit as it has no space after it!
-        for battery_index in range(0, len(battery)-1):
-            digit = int(battery[battery_index])
-
-            if digit != high_val:
-                continue
-
-            first_digit = digit
-            first_digit_pos = battery_index
-            break
-
-    # now find the highest digit after that first digit
-    second_digit = 0
-    for high_val in range(9, 0, -1):
-        if second_digit > 0:
-            break
-
-        for battery_index in range(first_digit_pos+1, len(battery)):
-            digit = int(battery[battery_index])
-
-            if digit != high_val:
-                continue
-
-            second_digit = digit
-            break
-
-    number = first_digit * 10 + second_digit
+        
+        remaining = num_digits_wanted - len(digits)
+        digit,offset = get_next_digit(battery, offset, remaining)
+        digits.append(digit)
+        
+    number = 0
+    for digit in digits:
+        number *= 10
+        number += digit
     total += number
 
 print(total)
