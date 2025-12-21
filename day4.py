612f0c7b221e1e8a069ab72c3527308f725c1a61
amy-phillips <amy@islovely.co.uk>
2025-12-21 18:09:18 +0000

    day 4 part 2

--- day4.py
+++ day4.py
@@ -9,33 +9,46 @@ def is_roll(lines, row_idx : int, col_idx : int):
         return False # fell off the side, doesn't count
     return lines[row_idx][col_idx] =='@'
 
+def remove_rolls(lines) -> int:
+    removed_count = 0
+    for row_idx in range(0, len(lines)):
+        row = lines[row_idx]
+        for col_idx in range(0, len(row)):
+            if not is_roll(lines, row_idx, col_idx):
+                continue
+
+            spot_count = 0
+            if is_roll(lines, row_idx, col_idx-1):
+                spot_count += 1
+            if is_roll(lines, row_idx, col_idx+1):
+                spot_count += 1
+            if is_roll(lines, row_idx-1, col_idx-1):
+                spot_count += 1
+            if is_roll(lines, row_idx-1, col_idx):
+                spot_count += 1
+            if is_roll(lines, row_idx-1, col_idx+1):
+                spot_count += 1
+            if is_roll(lines, row_idx+1, col_idx-1):
+                spot_count += 1
+            if is_roll(lines, row_idx+1, col_idx):
+                spot_count += 1
+            if is_roll(lines, row_idx+1, col_idx+1):
+                spot_count += 1
+
+            if spot_count < 4:
+                removed_count += 1
+                lines[row_idx][col_idx] = '.'
+    return removed_count
+
 total = 0
-lines = input.split('\n')
-for row_idx in range(0, len(lines)):
-    row = lines[row_idx]
-    for col_idx in range(0, len(row)):
-        if not is_roll(lines, row_idx, col_idx):
-            continue
+str_lines = input.split('\n')
+lines = []
+for line in str_lines:
+    lines.append(list(line))
 
-        spot_count = 0
-        if is_roll(lines, row_idx, col_idx-1):
-            spot_count += 1
-        if is_roll(lines, row_idx, col_idx+1):
-            spot_count += 1
-        if is_roll(lines, row_idx-1, col_idx-1):
-            spot_count += 1
-        if is_roll(lines, row_idx-1, col_idx):
-            spot_count += 1
-        if is_roll(lines, row_idx-1, col_idx+1):
-            spot_count += 1
-        if is_roll(lines, row_idx+1, col_idx-1):
-            spot_count += 1
-        if is_roll(lines, row_idx+1, col_idx):
-            spot_count += 1
-        if is_roll(lines, row_idx+1, col_idx+1):
-            spot_count += 1
-
-        if spot_count < 4:
-            total += 1
+removed_count = 1
+while removed_count > 0:
+    removed_count = remove_rolls(lines)
+    total += removed_count
 
 print(total)
