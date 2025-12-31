de08dc4105296925027bcffc6edf91c06473c409
amy-phillips <amy@islovely.co.uk>
2025-12-31 15:57:31 +0000

    day 9 part 1

--- day9.py
+++ day9.py
@@ -10,6 +10,18 @@ for line in input.split('\n'):
 
 max_area = 0
 
+# shift to start at the origin (this is actually part of part 2)
+min_corner = numpy.array([999999999,99999999])
+max_corner = numpy.array([0,0])
+for corner in corners:
+    min_corner[0] = min(min_corner[0], corner[0])
+    min_corner[1] = min(min_corner[1], corner[1])
+    max_corner[0] = max(max_corner[0], corner[0])
+    max_corner[1] = max(max_corner[1], corner[1])
+max_corner-=min_corner
+for corner in corners:
+    corner -= min_corner
+
 # brute force - in future might remove corners that are inside boxes?
 for idxa, cornera in enumerate(corners):
     for idxb in range(idxa+1, len(corners)):
