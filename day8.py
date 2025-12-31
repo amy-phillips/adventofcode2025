564b9681aa42842fc40952fa48ff2f9c47db260b
amy-phillips <amy@islovely.co.uk>
2025-12-31 15:11:51 +0000

    day 8 part 2

--- day8.py
+++ day8.py
@@ -12,54 +12,32 @@ def do_merge(seta, setb):
             seta.append(b)
     setb.clear()
 
-with open("day8input.txt","r") as f:
-    input = f.read()
-
-box_locations = []
-for line in input.split('\n'):
-    location = line.split(',')
-    box_locations.append(numpy.array([int(location[0]),int(location[1]),int(location[2])]))
-
-# index into this two dimensional array to find the distance between point_idx_a and point_idx_b
-distances = []
-for idxa,locationa in enumerate(box_locations):
-    distances.append([0]*len(box_locations))
-    for idxb in range(idxa+1, len(box_locations)):
-        locationb = box_locations[idxb]
-        loc_dist = numpy.sum(numpy.square(abs(locationa - locationb))) # scales as distance, no need to sqrt
-        distances[idxa][idxb] = loc_dist
-
-min_dist_in_set = 0
-box_sets = []
-for iteration in range(0, 1000):
-    min_dist_this_iteration = 99999999999999999
-    min_dist_indices = [0,0]
-    
+def calc_distances(box_locations):
+    distances = []
     for idxa,locationa in enumerate(box_locations):
+        distances.append([0]*len(box_locations))
         for idxb in range(idxa+1, len(box_locations)):
-            loc_dist = distances[idxa][idxb]
-            if loc_dist<min_dist_this_iteration and loc_dist>min_dist_in_set: # exclude previously joined pairs
-                min_dist_this_iteration = loc_dist
-                min_dist_indices = [idxa,idxb]
-    # join idxa/idxb
-    min_dist_in_set = min_dist_this_iteration # stash this to exclude this pair in future iterations so we don't repeat
+            locationb = box_locations[idxb]
+            loc_dist = numpy.sum(numpy.square(abs(locationa - locationb))) # scales as distance, no need to sqrt
+            distances[idxa][idxb] = loc_dist
+    return distances
+
+def add_to_sets(box_sets, min_dist_indices):
     added_to_set = False
     for set in box_sets:
         if min_dist_indices[0] in set:
             if not min_dist_indices[1] in set:
                 set.append(min_dist_indices[1])
-            added_to_set = True
-            break
+            return
         elif min_dist_indices[1] in set:
             if not min_dist_indices[0] in set:
                 set.append(min_dist_indices[0])
-            added_to_set = True
-            break
+            return
 
     if not added_to_set:
         box_sets.append(min_dist_indices)
 
-    # maybe we have new overlap and can merge sets
+def merge_sets(box_sets):
     merged = True
     while(merged):
         merged = False
@@ -71,14 +49,46 @@ for iteration in range(0, 1000):
                 do_merge(seta,setb)
                 merged = True
 
-sizes = []
-for set in box_sets:
-    sizes.append(len(set))
-sizes.sort(reverse=True)
+    for seta in box_sets:
+        if len(seta) == 0:
+            box_sets.remove([])
 
-multiplied = 1
-for idx in range(0,3):
-    multiplied *= sizes[idx]
+with open("day8input.txt","r") as f:
+    input = f.read()
 
-print(sizes)
+box_locations = []
+for line in input.split('\n'):
+    location = line.split(',')
+    box_locations.append(numpy.array([int(location[0]),int(location[1]),int(location[2])]))
+
+# index into this two dimensional array to find the distance between point_idx_a and point_idx_b
+distances = calc_distances(box_locations)
+
+min_dist_in_set = 0
+box_sets = []
+all_in_one_set = False
+min_dist_indices = [0,0]
+while not all_in_one_set:
+    min_dist_this_iteration = 99999999999999999
+    
+    for idxa,locationa in enumerate(box_locations):
+        for idxb in range(idxa+1, len(box_locations)):
+            loc_dist = distances[idxa][idxb]
+            if loc_dist<min_dist_this_iteration and loc_dist>min_dist_in_set: # exclude previously joined pairs
+                min_dist_this_iteration = loc_dist
+                min_dist_indices = [idxa,idxb]
+                
+    # join idxa/idxb
+    min_dist_in_set = min_dist_this_iteration # stash this to exclude this pair in future iterations so we don't repeat
+    add_to_sets(box_sets, min_dist_indices)
+
+    # maybe we have new overlap and can merge sets
+    merge_sets(box_sets)
+    
+    all_in_one_set = (len(box_sets[0]) == len(box_locations))
+
+# multiply the x co-ords of the last two points to be connected
+multiplied = box_locations[min_dist_indices[0]][0] * box_locations[min_dist_indices[1]][0]
+
+print(min_dist_indices)
 print(multiplied)
